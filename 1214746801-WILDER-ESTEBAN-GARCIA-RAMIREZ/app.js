a39ea573e61952d38ff2823ebabf68ddfce9138b
wigek <wilder.garciaramirez@gmail.com>
2025-09-27 11:36:45 -0500

    Fase 2: Implementadas funciones para alimentar y jugar con la mascota

--- 1214746801-WILDER-ESTEBAN-GARCIA-RAMIREZ/app.js
+++ 1214746801-WILDER-ESTEBAN-GARCIA-RAMIREZ/app.js
@@ -7,4 +7,27 @@ function mostrarEstado(){
     console.log(`El nivel de felicidad de ${nombreMascota} es: ${nivelFelicidad}`);
 }
 
+//mostrarEstado();
+
+const alimentar = function(puntosComida){
+    nivelHambre -= puntosComida;
+    if(nivelHambre <1){
+       nivelHambre = 0;
+       console.log(`¡${nombreMascota} está hambriento, debes alimentarlo ${nivelHambre}`);
+    }
+    console.log(`¡Ñam! El hambre de ${nombreMascota}  ha disminuido. Hambre: ${nivelHambre}`);
+}
+
+const jugar = (puntosDiversion) =>{
+    nivelFelicidad += puntosDiversion;
+    if (nivelFelicidad >99){
+        nivelFelicidad = 100;
+        console.log(`¡${nombreMascota} está muy feliz! Felicidad: ${nivelFelicidad}`);
+    }
+    console.log(`¡Qué divertido! La felicidad de ${nombreMascota} ha aumentado. Felicidad:${nivelFelicidad}/100.`)
+}
+
+
+alimentar(20);
+jugar(30);
 mostrarEstado();
