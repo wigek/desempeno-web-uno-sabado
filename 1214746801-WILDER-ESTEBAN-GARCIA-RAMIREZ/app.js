f000bc56217cecd67f437a83a9d1559a538dc34e
wigek <wilder.garciaramirez@gmail.com>
2025-09-27 11:56:09 -0500

    Fase 3: Añadida simulación de tiempo y diagnóstico de la mascota

--- 1214746801-WILDER-ESTEBAN-GARCIA-RAMIREZ/app.js
+++ 1214746801-WILDER-ESTEBAN-GARCIA-RAMIREZ/app.js
@@ -13,7 +13,7 @@ const alimentar = function(puntosComida){
     nivelHambre -= puntosComida;
     if(nivelHambre <1){
        nivelHambre = 0;
-       console.log(`¡${nombreMascota} está hambriento, debes alimentarlo ${nivelHambre}`);
+       console.log(`¡${nombreMascota} está muy lleno !: ${nivelHambre}`);
     }
     console.log(`¡Ñam! El hambre de ${nombreMascota}  ha disminuido. Hambre: ${nivelHambre}`);
 }
@@ -27,7 +27,48 @@ const jugar = (puntosDiversion) =>{
     console.log(`¡Qué divertido! La felicidad de ${nombreMascota} ha aumentado. Felicidad:${nivelFelicidad}/100.`)
 }
 
-
-alimentar(20);
+/*alimentar(20);
 jugar(30);
-mostrarEstado();
+mostrarEstado();*/
+
+const simularPasoDelTiempo=() =>{
+    for(let i=1; i<5; i++){
+        nivelHambre += 10;
+        nivelFelicidad -= 5;
+        if (nivelHambre >99){
+            console.log(`¡${nombreMascota} está muy hambriento! Hambre: ${nivelHambre}`);
+            nivelHambre = 0;
+        }
+        if (nivelFelicidad <0){
+            console.log(`¡${nombreMascota} está muy triste! Felicidad: ${nivelFelicidad}`);
+            nivelFelicidad = 0;
+        }
+    }
+    console.log(`Han pasado algunas horas... Hambre: ${nivelHambre}, Felicidad: ${nivelFelicidad}`);
+}
+
+//simularPasoDelTiempo();
+
+function diagnosticoMascota(){
+    if (nivelHambre >= 80){
+        console.log(`¡${nombreMascota} está muy hambriento! Debes alimentarlo.`);
+    } else if (nivelHambre >= 50){
+        console.log(`${nombreMascota} tiene algo de hambre. Considera alimentarlo pronto.`);
+    } else {
+        console.log(`${nombreMascota} está bien alimentado.`);
+    }
+
+    switch (nivelFelicidad){
+        case (nivelFelicidad >= 80):
+            console.log(`${nombreMascota} Tu mascota está muy feliz y juguetona.`);
+            break;
+        case (nivelFelicidad >= 40):
+            console.log(`${nombreMascota} Tu mascota está contenta`);
+            break;
+        default:
+            console.log(`${nombreMascota} Tu mascota se siente un poco triste`);
+    }
+}
+
+//simularPasoDelTiempo();
+//diagnosticoMascota();
